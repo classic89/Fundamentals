1a095320252b4b5d7435a248a59deb1f87abaa6e
Noe De La Mora <noedelamora@gmail.com>
2019-02-03 21:32:10 -0600

    Create multiples_of_three_or_five.py

---
+++ Python/Noe/Coding Challenges/multiples_of_three_or_five.py
@@ -0,0 +1,14 @@
+'''
+Kata Difficulty: 6
+runtime: O(N)
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+'''
+def multiple_sum(n):
+    return sum(i for i in range(0, n) if not i % 3 or not i % 5)
+
+
+print(multiple_sum(10))
