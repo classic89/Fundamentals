1b6e5ca4c891b210f3dd8382f8fa0a9811db0633
Noe De La Mora <noedelamora@gmail.com>
2019-02-05 17:56:34 -0600

    Create persistent_bugger.py

---
+++ Python/Noe/Coding Challenges/persistent_bugger.py
@@ -0,0 +1,20 @@
+'''
+Kata(6): https://www.codewars.com/kata/persistent-bugger
+runtime: O(N) N being the number of digits
+
+Write a function, persistence, that takes in a positive parameter num
+and returns its multiplicative persistence, which is the number of times
+you must multiply the digits in num until you reach a single digit.
+'''
+
+def persistence(n, iterations=0):
+    if n < 10:
+        return iterations
+    l = list(str(n))
+    result = 1
+    
+    for i in range(0, len(l)):
+        result *= int(l[i])
+    
+    return iterations+1 if result < 10 else persistence(result, iterations+1)
+
